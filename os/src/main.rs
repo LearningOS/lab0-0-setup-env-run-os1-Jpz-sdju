a755cd044bf266f45053078a184b5b18ee807bdd
jpz <rengeyouzh1@163.com>
2022-11-13 10:34:50 +0800

    update

--- os/src/main.rs
+++ os/src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
-    println!("Hello, world!");
-}
+#![no_main]
+#![no_std]
+mod lang_items;
+// fn main() {
+//     // println!("Hello, world!");
+// }
+use core::arch::global_asm;
+global_asm!(include_str!("entry.asm"));
